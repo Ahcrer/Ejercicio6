82f3ec255bda590f93b39e0ecbaa9f588c140c12
Brandon Cha <99994002+Ahcrer@users.noreply.github.com>
2023-09-28 17:56:30 -0700

    Sintaxis
    Se agregaron problemas de sintaxis, ortografía y se recorto un poco el código.

--- main.py
+++ main.py
@@ -11,29 +11,24 @@ while True:
     3) Multiplicar los dos números
     4) Dividir los dos números
     5) Cambiar los números elegidos
-    6) Exponente de un numero
+    6) Exponente de un número
     0) Apagar calculadora
     """)
     opcion = int(input("Elige una opción: ") )     
 
     if opcion == 1:
-        print(" ")
-        print("RESULTADO: La suma de ",n1,"+",n2," es igual a: ",n1+n2)
+        print("\nResultado: La suma de ",n1,"+",n2," es igual a: ",n1+n2)
     elif opcion == 2:
-        print(" ")
-        print("RESULTADO: La resta de ",n1,"-",n2,"es igual a: ",n1-n2)
+        print("\nResultado: La resta de ",n1,"-",n2,"es igual a: ",n1-n2)
     elif opcion == 3:
-        print(" ")
-        print("RESULTADO: El producto de ",n1,"*",n2," es igual a: ",n1*n2)
+        print("\nResultado: El producto de ",n1,"*",n2," es igual a: ",n1*n2)
     elif opcion == 4:
-        print("")
-        print("RESULTADO: La división de ",n1,"/",n2," es igual a : ", n1/n2)
+        print("\nResultado: La división de ",n1,"/",n2," es igual a : ", n1/n2)
     elif opcion == 5:
         n1 = float(input("Introduce tu primer número: ") )
         n2 = float(input("Introduce tu segundo número: ") )
-    elif opcion ?? 6:
-        print("")
-        print("RESULTADO: El numero: ",n1," exponenciado a la: ",n2," es igual a: ", n1**n2)
+    elif opcion == 6:
+        print("\nResultado: El numero: ",n1," exponenciado a la: ",n2," es igual a: ", n1**n2)
     elif opcion == 0:
         break
     else:
